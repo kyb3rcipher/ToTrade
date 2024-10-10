38ad5282564e5cf01e967a16bd3e9e4e13d0e685
Kyb3r Cipher <me@kyb3rcipher.com>
2024-10-10 17:20:33 -0600

    Add Inspo "Intro Acitivity"

---
+++ app/src/main/java/com/kyb3rcipher/hackathon/Activity/IntroActivity.java
@@ -0,0 +1,35 @@
+package com.kyb3rcipher.hackathon.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.kyb3rcipher.hackathon.R;
+import com.kyb3rcipher.hackathon.databinding.ActivityIntroBinding;
+
+public class IntroActivity extends BaseActivity {
+    ActivityIntroBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.goBtn.setOnClickListener(v -> startActivity(new Intent(IntroActivity.this, MainActivity.class)));
+    }
+}
