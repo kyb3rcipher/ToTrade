5506070b44cf5ea0ab361da648ee85059b787e46
joel <enajo006@gmail.com>
2024-10-11 02:53:05 -0600

    Add Detail Ui

---
+++ app/src/main/java/com/kyb3r/gototrade/Activity/DetailActivity.java
@@ -0,0 +1,17 @@
+package com.kyb3r.gototrade.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kyb3r.gototrade.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+    }
+}
